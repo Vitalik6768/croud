32976a3727a0beafe0a1f4d4f4bc0dd6f3f837c7
vitalik <vitalgreg1988@gmail.com>
2024-03-22 14:08:13 +0200

    test

---
+++ croudc/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace croudc.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ViewResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Login(FormCollection values)
+        {
+            return View();
+        }
+    }
+}
